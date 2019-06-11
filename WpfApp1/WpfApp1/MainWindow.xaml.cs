9713f6b29890b44e29052b3e1b59385445138c5a
luckyxiaohei <1440884465@qq.com>
2019-06-11 13:04:53 +0800

    hw3

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Tip tip;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tip = new Tip();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void performCalculation()
+        {
+            var selectedRadio = myStackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+            if (selectedRadio == null)
+            {
+              
+            }
+            else
+            {
+                double billAmount = 0.0;
+
+                string m = string.Format("{0:C}", billAmount);
+               
+
+
+                tip.CalculateTip(billAmountTextBox.Text, double.Parse(selectedRadio.Tag.ToString()));
+
+
+
+
+                if (tip.TipAmount!=m)
+                {
+                    amountToTipTextBlock.Text = tip.TipAmount;
+                    totalTextBlock.Text = tip.TotalAmount;
+                    
+                }
+                else  if(billAmountTextBox.Text != "")
+                {
+                   
+                    double billa;
+                    string bat = billAmountTextBox.Text;
+                    MessageBox.Show(bat);
+                    string ba = string.Format(new System.Globalization.CultureInfo("en-US"),"{0:C}", billAmountTextBox.Text);
+                    MessageBox.Show(ba);
+
+                    string baa = ba.Replace("$", "");
+                    MessageBox.Show(baa);
+
+                    billa = double.Parse(baa);
+                    
+                   
+                    
+                        double p = double.Parse(selectedRadio.Tag.ToString());
+
+                       
+       
+                        double tipa = p * billa;
+                        double am = billa + tipa;
+
+
+
+                        amountToTipTextBlock.Text = string.Format(new System.Globalization.CultureInfo("en-US"),"{0:C}", tipa);
+                        totalTextBlock.Text = string.Format(new System.Globalization.CultureInfo("en-US"),"{0:C}", am);
+
+
+
+                      
+                    
+                }
+
+            }
+
+
+        }
+
+        private void amountTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+           
+            
+            billAmountTextBox.Text = tip.BillAmount;
+        }
+
+        private void amountTextBox_MouseLeave(object sender, MouseEventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void amountTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            billAmountTextBox.Text = "";
+        }
+
+        private void billAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            performCalculation();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ WpfApp1/WpfApp1/Tip.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Tip
+    {
+        public string BillAmount { get; set; }
+        public string TipAmount { get; set; }
+        public string TotalAmount { get; set; }
+
+        public  Tip()
+        {
+            this.BillAmount = String.Empty;
+            this.TipAmount = String.Empty;
+            this.TotalAmount = String.Empty;
+        }
+
+        public void CalculateTip(string originalAmount, double tipPercentage)
+        {
+            double billAmount = 0.0;
+            double tipAmount = 0.0;
+            double totalAmount = 0.0;
+
+            if (double.TryParse(originalAmount.Replace('$', ' '), out billAmount))
+            {
+                tipAmount = billAmount * tipPercentage;
+                totalAmount = billAmount + tipAmount;
+            }
+            CultureInfo c = new CultureInfo("en-US");
+
+
+            this.BillAmount = billAmount.ToString("C", c);
+            this.TipAmount =  tipAmount.ToString("C", c);
+            this.TotalAmount =totalAmount.ToString("C", c);
+
+        }
+    }
+}
